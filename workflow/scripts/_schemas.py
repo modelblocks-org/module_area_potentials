dd34d21447985047017eb4f325d762bb4ece1e53
Stefan Pfenninger <stefan@pfenninger.org>
2025-11-17 09:42:37 +0100

    Improve schema

--- workflow/scripts/_schemas.py
+++ workflow/scripts/_schemas.py
@@ -1,25 +1,28 @@
 """Schemas for tabular data used in the workflow."""
 
-from pandera.pandas import DataFrameModel, Field, check
+import pandera.pandas as pa
 from pandera.typing.geopandas import GeoSeries
 from pandera.typing.pandas import Series
-from shapely.geometry import Point
 
 
-class Shapes(DataFrameModel):
+class ShapesSchema(pa.DataFrameModel):
+    """Schema for geographic shapes."""
+
+    shape_id: Series[str] = pa.Field(unique=True)
+    "A unique identifier for this shape."
+    country_id: Series[str]
+    "Country ISO alpha-3 code."
+    shape_class: Series[str] = pa.Field(isin=["land", "maritime"])
+    "Identifier of the shape's context."
+    geometry: GeoSeries
+    "Shape (multi)polygon."
+    parent_name: Series[str] | None
+    "Human-readable name in the parent dataset."
+
+    @pa.check("geometry", element_wise=True)
+    def check_geometries(cls, geom):
+        return (geom is not None) and (not geom.is_empty) and geom.is_valid
+
     class Config:
         coerce = True
         strict = False
-
-    shape_id: Series[str] = Field(unique=True)
-    "Unique ID for this shape."
-    country_id: Series[str]
-    "ISO alpha-3 code."
-    shape_class: Series[str] = Field(isin=["land", "maritime"])
-    "Shape classifier"
-    geometry: GeoSeries[Point] = Field()
-    "Shape polygon."
-
-    @check("geometry", element_wise=True)
-    def geom_not_empty(cls, geom):
-        return (geom is not None) and (not geom.is_empty) and geom.is_valid
